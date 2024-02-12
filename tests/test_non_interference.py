69900c182383774e39d987ef846bc10608bb82d5
jhnnsrs <jhnnsrs@gmail.com>
2024-02-12 18:13:30 +0100

    up

--- tests/test_non_interference.py
+++ tests/test_non_interference.py
@@ -5,12 +5,6 @@ from mikro_napari.widgets.main_widget import MikroNapariWidget
 @pytest.mark.qt
 def test_viewer_non_intrusive(make_napari_viewer, qtbot):
     viewer = make_napari_viewer()
-
-
-
-
-
-    
     widget = MikroNapariWidget(viewer)
     viewer.window.add_dock_widget(widget, area="left", name="Mikro")
 
